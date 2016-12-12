4f82097f3f606a9f836a8cf504dfb77530e7d606
Daniel Deutsch <deudan1010@gmail.com>
2016-12-12 12:04:39 +0100

    declaring a winner

--- reactTictactoe.jsx
+++ reactTictactoe.jsx
@@ -16,6 +16,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculatedWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'K' : 'D';
     this.setState({
       squares: squares,
@@ -32,7 +35,12 @@ class Board extends React.Component {
     );
   }
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'K' : 'D');
+    const winner = calculateWinner(this.state.squares);
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'K' : 'D');
+    }
     return (
       <div>
         <div className="status">{status}</div>
